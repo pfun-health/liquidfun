323f6f4f0321745ff058b01fd2a7a4cffd3b44bf
Robbie Capps <rocapp@gmail.com>
2025-10-18 18:09:13 -0700

    began setup with flake.nix (not yet successfully compiling)

--- flake.nix
+++ flake.nix
@@ -3,20 +3,48 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
 
-  outputs = { nixpkgs, ... }:
+  outputs = { self, nixpkgs, ... }@inputs:
     let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
+
+      inherit (self) outputs;
+
+      supportedSystems = [ "x86_64-linux" ];
+
+      forEachSupportedSystem = f:
+        nixpkgs.lib.genAttrs supportedSystems
+        (system: f { pkgs = import nixpkgs { inherit system; }; });
+
+      # This is a function that generates an attribute by calling a function you
+      # pass to it, with each system as an argument
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
     in {
-      devShells.default = pkgs.mkShell {
-        buildInputs = [ pkgs.cmake pkgs.mesa pkgs.libGLU pkgs.makeWrapper ];
-        shellHook = ''
-          echo "LiquidFun development shell. Use cmake and make to build:"
-          echo -e "
-               cmake -G 'Unix Makefiles'
-               make
-          "
-        '';
-      };
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.cmake
+            pkgs.mesa
+            pkgs.libGLU
+            pkgs.makeWrapper
+            pkgs.xorg.libX11
+            pkgs.xorg.libXext
+            pkgs.xorg.libXrender
+            pkgs.xorg.libXfixes
+            pkgs.xorg.libXdamage
+            pkgs.xorg.libXxf86vm
+            pkgs.xorg.libxcb
+            pkgs.python3
+            pkgs.gcc
+            pkgs.emscripten
+          ];
+          shellHook = ''
+            echo "LiquidFun development shell. Use cmake and make to build:"
+            echo -e "
+                 cmake -G 'Unix Makefiles'
+                 make
+            "
+          '';
+        };
+      });
     };
 }
